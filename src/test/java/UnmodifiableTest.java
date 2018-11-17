3b0a3b7538dc0a2673e7a120620dad7bcb007a41
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-17 23:10:19 +0100

    Initial commit

---
+++ src/test/java/UnmodifiableTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mtumilowicz on 2018-11-17.
+ */
+public class UnmodifiableTest {
+    @Test
+    public void isMutable() {
+//        given
+        var list = new LinkedList<Integer>();
+
+//        when
+        Collection<Integer> integers = Collections.unmodifiableCollection(list);
+//        then
+        assertTrue(integers.isEmpty());
+
+//        when
+        list.add(1);
+//        then
+        assertFalse(integers.isEmpty());
+    }
+
+    @Test
+    public void isMutable_fix() {
+//        given
+        var list = new LinkedList<Integer>();
+//        when
+        Collection<Integer> integers = Collections.unmodifiableCollection(new ArrayList<>(list));
+//        then        
+        assertTrue(integers.isEmpty());
+        
+//        when
+        list.add(1);
+//        then        
+        assertTrue(integers.isEmpty());
+    }
+}
